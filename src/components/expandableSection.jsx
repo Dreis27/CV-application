842da9ad9241b24aff9d7c7dbe0351bebdca0cfc
Andrejs <127331717+Dreis27@users.noreply.github.com>
2023-09-15 18:31:57 +0200

    fix syntax error in 'Expandable Section'

--- src/components/expandableSection.jsx
+++ src/components/expandableSection.jsx
@@ -4,7 +4,7 @@ function ExpandableSection({isOpen, setOpen, sectionName, iconName}) {
     return (
         <button
             className="expandable-section"
-            onClick={{()=> setOpen(isOpen ? "" : sectionName)}}
+            onClick={() => setOpen(isOpen ? "" : sectionName)}
         >
             <h2 className="expandable-section-header">
                 <i className={`${iconName}`}/>
