a300b2bc8a189ec56797263e942b0b288027bb69
Andrejs <127331717+Dreis27@users.noreply.github.com>
2023-09-17 21:35:54 +0200

    work on Education form components

--- src/components/education/addEducationSection.jsx
+++ src/components/education/addEducationSection.jsx
@@ -1,3 +1,44 @@
 import DisplaForms from "../displayForms";
 import ExpandableSection from "../expandableSection";
-import ButtonCreateForm from "../buttonCreateForm"
+import ButtonCreateForm from "../buttonCreateForm";
+import EducationForm from "./educationForm";
+
+function AddEducationSection({
+    educations,
+    isOpen,
+    onChange,
+    createForm,
+    setOpen,
+    onCancel,
+    toggleCollapsed,
+    onHide,
+    onRemove,
+}) {
+    return (
+        <div className="add-education-section section">
+            <ExpandableSection
+                isOpen={isOpen}
+                setOpen={setOpen}
+                sectionName="Education"
+                iconName="fa-solid fa-graduate-cap"    
+            />
+            <div className={`section-content ${isOpen ? "open" : ""}`}>
+                <DisplaForms
+                    forms={educations}
+                    FormComponent={EducationForm}
+                    onChange={onChange}
+                    onCancel={onCancel}
+                    onHide={onHide}
+                    onRemove={onRemove}
+                    toggleCollapsed={toggleCollapsed}
+                    titleKey="schoolName"
+                    arrayName="educations"
+                />
+
+                <ButtonCreateForm onClick={createForm} buttonText="Education" />
+            </div>
+        </div>
+    );
+}
+
+export default AddEducationSection;
