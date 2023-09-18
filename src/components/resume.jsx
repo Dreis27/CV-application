185bef4c7ca0ecf53b5575d6069a02e05185fa49
Andrejs <127331717+Dreis27@users.noreply.github.com>
2023-09-18 18:20:34 +0200

    create resume section

---
+++ src/components/resume.jsx
@@ -0,0 +1,25 @@
+import PersonalInfoSection from "./personal-info/personalInfoSection";
+import EducationInfoSection from "./education/educationInfoSection";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import ExperienceInfoSection from "./experience/experienceInfoSection";
+
+function Resume({ personalInfo, sections }) {
+    return (
+        <div className="resume-container">
+            <div className="resume">
+                <PersonalInfoSection
+                    fullName={personalInfo.fullName}
+                    email={personalInfo.email}
+                    phoneNumber={personalInfo.phoneNumber}
+                    address={personalInfo.address}
+                />
+                <div>
+                    <EducationInfoSection educations={sections.educations} />
+                    <ExperienceInfoSection experiences={sections.experiences} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Resume;
