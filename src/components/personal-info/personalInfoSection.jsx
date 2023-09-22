1790ce3951a2ed36ac0b1ecc54fb6ee839a10f9c
Andrejs <127331717+Dreis27@users.noreply.github.com>
2023-09-22 20:59:40 +0200

    Add extra components

---
+++ src/components/personal-info/personalInfoSection.jsx
@@ -0,0 +1,29 @@
+
+
+export default function PersonalInfoSection({ email, fullName, phoneNumber, address }){
+    return (
+        <div className="personal-info">
+            <h1 className="resume-name">{fullName}</h1>
+            <div className="contact-info">
+                {address && (
+                    <div>
+                        <b>Address</b><br/>
+                        {address}
+                    </div>
+                )}
+                {phoneNumber && (
+                    <div>
+                        <b>Phone </b>
+                        {phoneNumber}
+                    </div>
+                )}
+                {email && (
+                    <div>
+                        <b>E-mail </b>
+                        {email}
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+}
