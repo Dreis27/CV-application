1b6237daa3352102faa216c7bf9976d13a6881db
Andrejs <127331717+Dreis27@users.noreply.github.com>
2023-09-28 08:10:02 +0200

    create 'example-data' object

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,10 @@
+import { useState } from "react";
+import PersonalInfo from "./components/personal-info/personalInfo";
+import AddEducationSection from "./components/education/addEducationSection";
+import AddExperienceSection from "./components/experience/addExperienceSection";
+import Resume from "./components/resume";
+import uniqid from "uniqid";
+import TemplateLoader from "./components/templateLoader";
+import exampleData from "./example-data";
 
 
