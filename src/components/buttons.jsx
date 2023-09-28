b4fa18e1e63e2330e38c81be867622e1ad4c54d2
Andrejs <127331717+Dreis27@users.noreply.github.com>
2023-09-28 09:59:13 +0200

    add 'buttons' component

---
+++ src/components/buttons.jsx
@@ -0,0 +1,20 @@
+
+
+export default function Buttons ({onCancel, onSave, onRemove}) {
+    return (
+        <div className="buttons">
+            <button className="delete" onClick={onRemove} type='button'>
+                <i className="fa-solid fa-trash" />
+                Delete
+            </button>
+            <div className="main-buttons">
+                <button className="cancel" onClick={onCancel} type="button">
+                    Cancel
+                </button>
+                <button className="save" onClick={onSave} type="submit">
+                    Save
+                </button>
+            </div>
+        </div>
+    )
+}
