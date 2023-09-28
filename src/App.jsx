e1020d812b780372e16fc35b1d9b75096823edcb
Andrejs <127331717+Dreis27@users.noreply.github.com>
2023-09-28 08:27:48 +0200

    Build base skeleton of App

--- src/App.jsx
+++ src/App.jsx
@@ -8,3 +8,56 @@ import TemplateLoader from "./components/templateLoader";
 import exampleData from "./example-data";
 
 
+export default function App() {
+    const [personalInfo, setPersonalInfo] = useState(exampleData.personalInfo);
+    const [sections, setSections] = useState(exampleData.sections);
+    const [sectionOpen, setSectionOpen] = useState(null);
+    const [prevState, setPrevState] = useState(null);
+
+    return (
+        <div className="app">
+            <div className="edit-side">
+                <div className="form-container">
+                    <TemplateLoader
+                        onClear={}
+                    />
+                    <PersonalInfo
+                        onChange={}
+                        fullName={}
+                        email={}
+                        phoneNumber={}
+                        address={}
+                    />
+                    <AddEducationSection
+                        educations={}
+                        isOpen={}
+                        onChange={}
+                        createForm={}
+                        setOpen={}
+                        onCancel={}
+                        toggleCollapsed={}
+                        onRemove={}
+                        onDelete={}
+                    />
+                    <AddExperienceSection
+                        experiences={}
+                        isOpen={}
+                        onChange={}
+                        createForm={}
+                        setOpen={}
+                        onCancel={}
+                        toggleCollapsed={}
+                        onRemove={}
+                        onDelete={}
+                    />                        
+                </div>
+            </div>
+
+            <Resume
+                personalInfo={personalInfo}
+                sections={sections}
+            />
+        </div>
+    )
+}
+
