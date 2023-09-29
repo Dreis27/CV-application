c3f46fd46222a35f26fc521fa0b161f2d1b5cbfa
Andrejs <127331717+Dreis27@users.noreply.github.com>
2023-09-29 20:01:03 +0200

    bug fixes

--- src/components/displaySection.jsx
+++ src/components/displaySection.jsx
@@ -2,12 +2,22 @@ import React from "react";
 
 export default function DisplaySection ({array, InfoComponent, title}) {
     return (
-        <React.Fragment>
-            {array.length && ( <h3 className="header-text">{title}</h3> )}
-
-            {
-            array.length && array.map((info) => ( <InfoComponent info={info} key={info.id}/>))
-            }
-        </React.Fragment>
-    )
-}
+        <>
+          {array.length > 0 && (
+            <>
+              <h3 className="header-text">{title}</h3>
+              {array.map((info) => (
+                <InfoComponent info={info} key={info.id} />
+              ))}
+            </>
+          )}
+        </>
+      );
+    }
+    
+    
+    
+    
+    
+    
+    
